69903d9108577e6382516baf672ae6fd5d7df376
pr0f3ss0r <pr0f3ss0r.h@gmail.com>
2021-04-15 10:30:27 +0800

    Final

---
+++ app/src/main/java/com/tosindroid/pr0f/holyquran/Chapters.java
@@ -0,0 +1,31 @@
+package com.tosindroid.pr0f.holyquran;
+
+public class Chapters {
+    private String surahs;
+    private String pageNumber;
+    private String chapterNumber;
+
+    public String getSurahs() {
+        return surahs;
+    }
+
+    public void setSurahs(String surahs) {
+        this.surahs = surahs;
+    }
+
+    public String getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(String pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public String getChapterNumber() {
+        return chapterNumber;
+    }
+
+    public void setChapterNumber(String chapterNumber) {
+        this.chapterNumber = chapterNumber;
+    }
+}
